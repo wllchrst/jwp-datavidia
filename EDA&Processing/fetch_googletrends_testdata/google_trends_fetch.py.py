95751ff5aa26c3986cfcd1df4f4997860f9d7267
William Christian <wllchrst@outlook.com>
2025-03-11 11:26:09 +0700

    fix: add additional code for ensuring every thing is fetched

--- EDA&Processing/fetch_googletrends_testdata/google_trends_fetch.py.py
+++ EDA&Processing/fetch_googletrends_testdata/google_trends_fetch.py.py
@@ -3,8 +3,13 @@ import pandas as pd
 from time import sleep
 from random import randint
 
-# Inisialisasi Pytrends
-pytrends = TrendReq(hl='id', tz=420)
+requests_args = {
+'headers': {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    }
+}
+
+pytrends = TrendReq(retries=3, hl='id', tz=420, requests_args=requests_args)
 
 # Daftar bahan pangan
 keywords = [
@@ -13,39 +18,36 @@ keywords = [
     "gula", "minyak goreng", "telur ayam", "tepung", "tepung terigu"
 ]
 
-# Parameter pencarian
-timeframe = "2024-10-01 2024-12-31"
-geo = "ID"  # Indonesia
 
-# Dictionary untuk menyimpan hasil
+timeframe = "2024-10-01 2024-12-31"
+geo = "ID"
+
 all_data = {}
 
-# Looping untuk tarik data per keyword dengan delay acak
-for keyword in keywords:
-    print(f"Mengambil data untuk: {keyword}...")
-    2
-    try:
-        pytrends.build_payload([keyword], timeframe=timeframe, geo=geo)
-        data = pytrends.interest_over_time()
+while len(keywords) > 0:
+    for keyword in keywords:
+        print(f"Mengambil data untuk: {keyword}...")
         
-        if not data.empty:
-            all_data[keyword] = data[keyword]
-            print(f"✅ Data untuk {keyword} berhasil diambil.")
-        else:
-            print(f"⚠️ Data kosong untuk {keyword}.")
-    
-    except Exception as e:
-        print(f"❌ Error saat mengambil data {keyword}: {e}")
-    
-    # Delay acak antara 10-30 detik untuk menghindari blokir
-    delay = randint(25, 75)
-    print(f"Menunggu {delay} detik sebelum request berikutnya...\n")
-    sleep(delay)
+        try:
+            pytrends.build_payload([keyword], timeframe=timeframe, geo=geo)
+            data = pytrends.interest_over_time()
+            
+            if not data.empty:
+                all_data[keyword] = data[keyword]
+                print(f"✅ Data untuk {keyword} berhasil diambil.")
+            else:
+                print(f"⚠️ Data kosong untuk {keyword}.")
+                
+            keywords.remove(keyword)
+        
+        except Exception as e:
+            print(f"❌ Error saat mengambil data {keyword}: {e}")
 
-# Gabungkan semua data ke dalam satu DataFrame
+    print('Delaying 10 seconds')
+    sleep(10)
+
 trends_df = pd.DataFrame(all_data)
 
-# Simpan ke file CSV
 trends_df.to_csv("./google_trends_test_data.csv")
 
 print("✅ Semua data telah berhasil disimpan dalam 'google_trends_bahan_pangan.csv'")
